3c6f4b2c631c87f56ed397a9ed6a46ad89f0b840
Ilya <rewasd91@mail.ru>
2017-03-31 13:29:30 +0300

    Правки №7

--- www/index.php
+++ www/index.php
@@ -10,7 +10,7 @@
 </head>
 
 <body> 
-
+<div class="page">
 <?php include "blocks/header.php"; ?>
 
 <section class="actions actions_main">
@@ -670,7 +670,7 @@
 </section>
 
 <?php include "blocks/footer.php"; ?>
-
+</div>
 <?php include "blocks/forms.php"; ?>
 <?php include "blocks/scripts.php"; ?>
 </body>
