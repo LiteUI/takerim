14473b4a3a2c0f226135f1b1b22a31032761d8a6
Gleb Gorokhov <gg@verstaburo.com>
2017-04-29 18:56:19 +0300

    history

---
+++ www/blocks/ebay/ebay-history.php
@@ -0,0 +1,118 @@
+<section class="ebay-section">
+	<div class="container">
+  		<div class="white-padding-section">
+  			<!-- Шапка блока -->
+  			<div class="table-header">
+  				<h3 class="ebay-heading table-header__heading">История продаж продавца</h3>
+  				
+  				<div class="ebay-table ebay-table_big table-header__table">
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Продано товаров</p>
+  						<p class="ebay-table__value">147</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Не продано товаров</p>
+  						<p class="ebay-table__value">4</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Объем продаж</p>
+  						<p class="ebay-table__value">$9266.00</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Последняя продажа</p>
+  						<p class="ebay-table__value">$0.00</p>
+  					</div>
+  				</div>
+  			</div>
+  			
+  			<!-- Основная таблица -->
+  			<div class="ebay-table-wrapper">
+  				<!-- Строка -->
+  				<div class="ebay-table ebay-table_row ebay">
+  					<div class="ebay-table__col ebay-table__col_imagecol">
+  						<div class="ebay-table__image">
+  							<a href="#"><img src="img/goods4.png" alt="@@"></a>
+  						</div>
+  					</div>
+  					<div class="ebay-table__col ebay-table__col_flexible">
+  						<p class="ebay-table__heading">905273065351</p>
+  						<p class="ebay-table__value"><a href="#">Сбалансированные беспроводные наушники Gravity</a></p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Qtw</p>
+  						<p class="ebay-table__value">10</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Продаж</p>
+  						<p class="ebay-table__value">10</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Ставка</p>
+  						<p class="ebay-table__value">—</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Покупатель</p>
+  						<p class="ebay-table__value">H***a</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Цена</p>
+  						<p class="ebay-table__value">$25.57</p>
+  						<p class="ebay-table__subvalue">Доставка $0.00</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Дата окончания</p>
+  						<p class="ebay-table__value">07.04.2017</p>
+  						<p class="ebay-table__subvalue">08:23:09</p>
+  					</div>
+  				</div>
+  				
+  				<!-- Строка -->
+  				<div class="ebay-table ebay-table_row ebay">
+  					<div class="ebay-table__col ebay-table__col_imagecol">
+  						<div class="ebay-table__image">
+  							<a href="#"><img src="img/goods9.png" alt="@@"></a>
+  						</div>
+  					</div>
+  					<div class="ebay-table__col ebay-table__col_flexible">
+  						<p class="ebay-table__heading">4935479524565</p>
+  						<p class="ebay-table__value"><a href="#">Инновационный мини холодильник OOINO Mini Refrigerator</a></p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Qtw</p>
+  						<p class="ebay-table__value">10</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Продаж</p>
+  						<p class="ebay-table__value">10</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Ставка</p>
+  						<p class="ebay-table__value">—</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Покупатель</p>
+  						<p class="ebay-table__value">H***a</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Цена</p>
+  						<p class="ebay-table__value">$25.57</p>
+  						<p class="ebay-table__subvalue">Доставка $0.00</p>
+  					</div>
+  					<div class="ebay-table__col">
+  						<p class="ebay-table__heading">Дата окончания</p>
+  						<p class="ebay-table__value">07.04.2017</p>
+  						<p class="ebay-table__subvalue">08:23:09</p>
+  					</div>
+  				</div>
+  			</div>
+  			
+  			<!-- Показать еще -->
+  			<div class="ebay-showmore">
+  				<a href="#" class="button button_grey">Показать еще</a>
+  			</div>
+  			
+  			<!-- Розовая кнопка -->
+  			<span class="ebay-float-button"></span>
+  		</div>
+	</div>
+</section>
